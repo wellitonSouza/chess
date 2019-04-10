43b35429b632f03d045c16528eb52ab1741c2256
Welliton Souza <wellitonaz@yahoo.com.br>
2019-04-09 22:12:37 -0300

    Exception

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -3,14 +3,17 @@ package boardgame;
 public class Board {
 
 	private int rows;
-	private int column;
+	private int columns;
 	private Piece[][] pieces;
 	
 	
-	public Board(int rows, int column) {
+	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1 ) {
+			throw new BoardException("Erro ao criar tabuleiro : O valor não pode ser menor que 1");
+		}
 		this.rows = rows;
-		this.column = column;
-		pieces  = new Piece[rows][column];
+		this.columns = columns;
+		pieces  = new Piece[rows][columns];
 	}
 
 
@@ -19,31 +22,44 @@ public class Board {
 	}
 
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-
 	public int getColumn() {
-		return column;
-	}
-
-
-	public void setColumn(int column) {
-		this.column = column;
+		return columns;
 	}
 	
 	public Piece piece(int row, int column) {
 		return pieces[row][column];
 	}
 	
-	public Piece Piece(Position position) {
+	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição não encontrada no tabuleiro!");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Já existem uma peça na posição "+ position);
+		}
 		pieces[position.getRow()][position.getColumn()]= piece;
 		piece.position = position;
 	}
+	
+	public boolean positionExists(int row, int column) {
+		
+		return rows >=0  && row < rows && column >=0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(),position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição não encontrada no tabuleiro!");
+		}
+		return piece(position) != null;
+	}
+	
 }
